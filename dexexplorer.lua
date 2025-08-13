1efb5723e7dc7278fbbd94ec548c766f40413846
BloxVault | Official <hd.ahles07@outlook.com>
2025-08-14 01:50:01 +0200

    Update dexexplorer.lua

--- dexexplorer.lua
+++ dexexplorer.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SirMiloRBLX2/scripts/refs/heads/main/DexMobile.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xingtaiduan/Script/refs/heads/main/Tools/DexMobile.lua"))()
