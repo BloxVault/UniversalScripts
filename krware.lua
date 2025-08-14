c54374a00f7842b09dcbc3992f5462f87ff88937
BloxVault | Official <hd.ahles07@outlook.com>
2025-08-15 01:29:20 +0200

    Update krware.lua

--- krware.lua
+++ krware.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KRWareHub/KRWare/refs/heads/main/KRWare%20Hub%20Loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KrWareScripts/KrWareNewList/main/Krware"))()
